812444d0c2e004c88b7bf7195bc17c49af0eb508
Inderprit Singh <singhinderprit1992@gmail.com>
2023-05-05 17:35:44 -0400

    updated some comments

--- server/db/index.js
+++ server/db/index.js
@@ -3,6 +3,8 @@ const { User } = require('./Models/User');
 const init = async () => {
     try {
         await db.sync({ force: true }); //typically we don't want to do force true in production mode.
+        //alter:true is another option for this as it will just modify existing tables dependent on if there were changes made while preserving the users in your database.
+        //alter:true is still considered risky to do so in a production environment. In a production environment you would typically use Sequelize migration tools.
         // await User.create({ firstName: 'John' });
 
         console.log('connected');
